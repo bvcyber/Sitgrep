1befc2d96122b670baad268007b26be615fbe0d9
John Ascher <jascher@securityinnovation.com>
2025-02-04 09:36:29 -0600

    Update to 3.6.9

--- src/utils/progressbar.py
+++ src/utils/progressbar.py
@@ -1,9 +1,9 @@
-
 from utils import messages as msg
 import importlib
 from git import RemoteProgress
 from rich.progress import Progress, BarColumn, TextColumn
 from rich.console import Console
+
 
 def hsv_to_rgb(h, s, v):
     """Converts HSV to RGB."""
@@ -33,6 +33,7 @@ def hsv_to_rgb(h, s, v):
     if i == 5:
         return v, p, q
 
+
 def rgb_to_hex(rgb):
     """Converts RGB to a hex color code."""
     return "#{:02X}{:02X}{:02X}".format(rgb[0], rgb[1], rgb[2])
@@ -41,38 +42,41 @@ def rgb_to_hex(rgb):
 class ProgressBar(RemoteProgress):
     def __init__(self, target="", hide=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-  
+
         self.progress = Progress(
             TextColumn("[bold blue]{task.description}"),
             BarColumn(),
             "[progress.percentage]{task.percentage:.2f}%",
-            transient=hide
+            transient=hide,
         )
         self.msg = f"Cloning {target}"
         self.task = self.progress.add_task(self.msg, total=100)
-        self.progress.start()
-        self.total_progress = 0 
-        self.total_max = 0  
+        self.total_progress = 0
+        self.total_max = 0
         self.count = 0
 
-    def update(self, op_code, cur_count, max_count=None, message=''):
+    def start(self):
+        self.progress.start()
+
+    def update(self, op_code, cur_count, max_count=None, message=""):
         if max_count:
 
             if self.total_max != max_count:
                 self.total_max = max_count
-        
-            self.progress.update(self.task, completed=cur_count, total=max_count, description=self.msg)
+
+            self.progress.update(
+                self.task, completed=cur_count, total=max_count, description=self.msg
+            )
 
             if cur_count == self.total_max and self.count >= 4:
-                self.progress.stop()
+                self.stop()
             elif cur_count == self.total_max:
                 self.count += 1
-         
-
 
     def finish(self):
         """Finish the progress bar manually if needed."""
         if self.task and self.progress:
-            self.progress.stop() 
+            self.stop()
 
-
+    def stop(self):
+        self.progress.stop()
