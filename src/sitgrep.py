e8664ab9a962601bdf8d7fa72fb407d679458fb7
John Ascher <jascher97@proton.me>
2026-04-20 16:23:21 -0500

    Fix package checking logic

--- src/sitgrep.py
+++ src/sitgrep.py
@@ -1211,22 +1211,28 @@ def get_packages_from_dir(dir):
         packages.append(config)
         foundConfig = True
 
+
     for folder in folders:
         os.chdir(folder)
-        if not foundConfig and os.path.isfile("sitgrep-config.json"):
+        if os.path.isfile("sitgrep-config.json"):
             config = json.loads(open("sitgrep-config.json", "r").read())
             packages.append(config)
-        elif not foundConfig:
-            packages.append(
-                {
-                    "path": dir,
-                    "project": folder,
-                    "branch": "",
-                    "site": "unknown",
-                    "user": None,
-                }
-            )
+            foundConfig = True
         os.chdir("../")
+
+    target = dir.split(os.sep)[-1]
+
+    if not foundConfig:
+        packages.append(
+            {
+                "path": dir,
+                "project": target,
+                "branch": "",
+                "site": "unknown",
+                "user": None,
+            }
+        )
+
     os.chdir("../")
     return packages
 
